73c582c96ccd235b070b822457c59d505c9f8a13
Piosek <piosek2006@gmail.com>
2025-10-15 13:57:46 +0200

    zad1

--- Lab2Programowanie Krzysiu.cpp
+++ Lab2Programowanie Krzysiu.cpp
@@ -2,19 +2,31 @@
 //
 
 #include <iostream>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    // Zadanie 1
+	/*for (int i = 0; i <= 10; i++)
+    {
+		cout << i << endl;
+	}
+	for (int i = 5; i <= 7; i++)
+	{
+		cout << i << endl;
+	}
+	for (int i = 5; i < 7; i++)
+	{
+		cout << i << endl;
+	}
+	for (int i = 0; i <= 10; i+=2)
+	{
+		cout << i << endl;
+	}
+	for (int i = 10; i >= 0; i--)
+	{
+		cout << i << endl;
+	}*/
+	return 0;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
