0cf50b448bfd22e4679162d55e475ec019322f43
Piosek <piosek2006@gmail.com>
2025-10-15 14:21:50 +0200

    zad7

--- Lab2Programowanie Krzysiu.cpp
+++ Lab2Programowanie Krzysiu.cpp
@@ -26,7 +26,66 @@ int main()
 	for (int i = 10; i >= 0; i--)
 	{
 		cout << i << endl;
+	}
+
+	// Zadanie 2, 3, 5
+	float fahr, celsius, start, limit, krok;
+
+	start = 0;
+	limit = 200;
+	krok = 20;
+
+	fahr = start;
+	while (fahr <= limit)
+	{
+		celsius = 5.0 * (fahr - 32.0) / 9.0;
+		cout << fahr << "\t" << celsius << endl;
+		fahr = fahr + krok;
+	}
+	
+	for (fahr = 0.0; fahr <= 200.0; fahr = fahr + 20.0)
+	{
+		celsius = 5.0 * (fahr - 32.0) / 9.0;
+		cout << fahr << "\t" << celsius << endl;
+	}
+	
+	for (fahr = 0; fahr <= 200; fahr = fahr + 20)
+	{
+		celsius = 5 * (fahr - 32.0) / 9;
+		cout << fahr << "\t" << celsius << endl;
+	}
+
+	// Zadanie 6
+	float fahr, celsius, start, limit, krok;
+
+	start = 0;
+	cout << "Podaj limit stopni F: ";
+	cin >> limit;
+	cout << "Podaj krok: ";
+	cin >> krok;
+
+	for (fahr = 0; fahr <= limit; fahr = fahr + krok)
+	{
+		celsius = 5 * (fahr - 32.0) / 9;
+		cout << fahr << "\t" << celsius << endl;
 	}*/
+
+	// Zadanie 7
+	float rzeczywista[10];
+
+	for(int i = 0; i < 10; i++)
+	{
+		cout << "Podaj liczbe: ";
+		cin >> rzeczywista[i];
+	}
+	for (int i = 0; i < 10; i++)
+	{
+		if (rzeczywista[i] > 0)
+		{
+			cout << "Liczby rzeczywiste: " << rzeczywista[i] << endl;
+		}
+	}
+
 	return 0;
 }
 
