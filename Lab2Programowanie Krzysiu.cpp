e96cc4e612586d9a51324e4a3586209fca27460c
Piosek <piosek2006@gmail.com>
2025-10-15 14:57:50 +0200

    zad 15

--- Lab2Programowanie Krzysiu.cpp
+++ Lab2Programowanie Krzysiu.cpp
@@ -2,6 +2,7 @@
 //
 
 #include <iostream>
+#include <cstdlib>
 using namespace std;
 
 int main()
@@ -68,7 +69,7 @@ int main()
 	{
 		celsius = 5 * (fahr - 32.0) / 9;
 		cout << fahr << "\t" << celsius << endl;
-	}*/
+	}
 
 	// Zadanie 7
 	float rzeczywista[10];
@@ -85,6 +86,103 @@ int main()
 			cout << "Liczby rzeczywiste: " << rzeczywista[i] << endl;
 		}
 	}
+
+	// Zadanie 8
+	for (int i = 0; i <= 100; i++)
+	{
+		if (i % 2 == 0)
+			cout << i << "\n";
+	}
+
+	// Zadanie 9
+	int n;
+	cout << "Podaj przez ile mam dzielic: ";
+	cin >> n;
+
+	for (int i = 0; i <= 100; i++)
+	{
+		if (i % n == 0)
+			cout << i << "\n";
+	}
+	
+	// Zadanie 10
+	int n;
+	int suma = 0;
+	cout << "Podaj przez ile mam dzielic: ";
+	cin >> n;
+
+	for (int i = 0; i <= 100; i++)
+	{
+		if (i % n == 0)
+			suma++;
+	}
+	cout << "Liczb podzielnych przez " << n << " jest: " << suma << endl;
+
+	// Zadanie 11
+	int a, b;
+	cout << "Podaj a: ";
+	cin >> a;
+	cout << "Podaj b: ";
+	cin >> b;
+	
+	if (a > b)
+	{
+		for (int i = b; i <= a; i++)
+		{
+			if (i % 3 == 0)
+				cout << i << "\n";
+		}
+	}
+	else if (a < b)
+	{
+		for (int i = a; i <= b; i++)
+		{
+			if (i % 3 == 0)
+				cout << i << "\n";
+		}
+	}
+	else
+	{
+		cout << "Liczby sa rowne";
+	}
+
+	// Zadanie 12
+	int n, srednia;
+	srednia = 0;
+	cout << "Podaj n: ";
+	cin >> n;
+	for (int i = 1; i <= n; i++)
+	{
+		srednia = srednia + i;
+	}
+	srednia = srednia / n;
+	cout << "Srednia arytmetyczna liczb od 1 do " << n << " wynosi: " << srednia << endl;
+
+	// Zadanie 13
+	for (int i = 100; i >= 0; i--)
+	{
+		cout << i << endl;
+	}
+
+	// Zadanie 14
+int a, b, c;
+cout << "Podaj 3 liczby:\n";
+cin >> a >> b >> c;
+if (a <= b && a <= c)
+{
+	cout << "Liczba " << a << " jest najmniejsza" << endl;
+}
+else if (b <= a && b <= c)
+{
+	cout << "Liczba " << b << " jest najmnniejsza" << endl;
+}
+else
+{
+	cout << "Liczba " << c << " jest najmniejsza" << endl;
+}*/
+
+// Zadanie 15
+
 
 	return 0;
 }
